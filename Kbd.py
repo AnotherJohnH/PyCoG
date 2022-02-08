18c324a45ffe015cfd1fb074355416fb40c7c17f
John D. Haughton <anotherjohnh@gmail.com>
2022-02-08 20:40:49 +0000

    Cosmetic white space

--- Kbd.py
+++ Kbd.py
@@ -12,43 +12,43 @@ buffer = None
 state  = 0
 
 def read(timeout = 0):
-  ''' Unbuffered char read '''
+   ''' Unbuffered char read '''
 
-  global buffer, state
+   global buffer, state
 
-  def readKeyPress():
-    fd    = sys.stdin.fileno()
-    attrs = termios.tcgetattr(fd)
-    tty.setcbreak(fd)
-    while True:
-        try:
-          buffer.put(sys.stdin.read(1))
-        except:
-          break
-    termios.tcsetattr(fd, termios.TCSADRAIN, attrs)
+   def readKeyPress():
+      fd    = sys.stdin.fileno()
+      attrs = termios.tcgetattr(fd)
+      tty.setcbreak(fd)
+      while True:
+         try:
+            buffer.put(sys.stdin.read(1))
+         except:
+            break
+      termios.tcsetattr(fd, termios.TCSADRAIN, attrs)
 
-  if not buffer:
-    buffer = queue.Queue()
-    threading.Thread(target=readKeyPress, daemon=True).start()
+   if not buffer:
+      buffer = queue.Queue()
+      threading.Thread(target=readKeyPress, daemon=True).start()
 
-  while True:
-    try:
-      ch = buffer.get(timeout=timeout)
-    except:
-      return NOTHING
+   while True:
+      try:
+         ch = buffer.get(timeout=timeout)
+      except:
+         return NOTHING
 
-    if state == 0:
-      if ch == '\033':
-        state = 1
-      else:
-        return ch
+      if state == 0:
+         if ch == '\033':
+            state = 1
+         else:
+            return ch
 
-    elif state == 1:
-      state = 2 if ch == '[' else 0
+      elif state == 1:
+         state = 2 if ch == '[' else 0
 
-    elif state == 2:
-      state = 0
-      if   ch == 'A': return UP
-      elif ch == 'B': return DOWN
-      elif ch == 'C': return RIGHT
-      elif ch == 'D': return LEFT
+      elif state == 2:
+         state = 0
+         if   ch == 'A': return UP
+         elif ch == 'B': return DOWN
+         elif ch == 'C': return RIGHT
+         elif ch == 'D': return LEFT
--- main.py
+++ main.py
@@ -21,64 +21,64 @@ bowmen = []
 arrows = []
 
 for _ in range(NUM_BOWMEN):
-  bowmen.append(Sprite(frame, Frame.CYAN, BOWMAN))
+   bowmen.append(Sprite(frame, Frame.CYAN, BOWMAN))
 
 # The game
 cycle = 0
 while True:
 
-  if not player.alive:
-    frame.shout(Frame.RED, 'YOU DIED')
-    break
+   if not player.alive:
+      frame.shout(Frame.RED, 'YOU DIED')
+      break
 
-  frame.redraw()
+   frame.redraw()
 
-  k = Kbd.read(timeout = 0.05)
+   k = Kbd.read(timeout = 0.05)
 
-  hit = ' '
-  if k == Kbd.UP:
-    hit = player.move(0, -1)
-  elif k == Kbd.DOWN:
-    hit = player.move(0, +1)
-  elif k == Kbd.LEFT:
-    hit = player.move(-1, 0)
-  elif k == Kbd.RIGHT:
-    hit = player.move(+1, 0)
+   hit = ' '
+   if k == Kbd.UP:
+      hit = player.move(0, -1)
+   elif k == Kbd.DOWN:
+      hit = player.move(0, +1)
+   elif k == Kbd.LEFT:
+      hit = player.move(-1, 0)
+   elif k == Kbd.RIGHT:
+      hit = player.move(+1, 0)
 
-  if hit == BOWMAN:
-    Sprite.listKillAt(bowmen, player.x, player.y, DEAD)
-  elif hit == ARROW:
-    player.kill(DEAD)
+   if hit == BOWMAN:
+      Sprite.listKillAt(bowmen, player.x, player.y, DEAD)
+   elif hit == ARROW:
+      player.kill(DEAD)
 
-  cycle += 1
-  if cycle < 5:
+   cycle += 1
+   if cycle < 5:
       continue
-  cycle = 0
+   cycle = 0
 
-  for arrow in arrows:
-    if arrow.num_moves == (frame.height - 2):
-       arrow.kill()
-    else:
-       hit = arrow.integrate(WRAP_ARROWS)
-       if hit == PLAYER:
-         player.kill(DEAD)
-       elif hit == BOWMAN:
-         Sprite.listKillAt(bowmen, arrow.x, arrow.y, DEAD)
-       elif hit == 'STUCK':
+   for arrow in arrows:
+      if arrow.num_moves == (frame.height - 2):
          arrow.kill()
+      else:
+         hit = arrow.integrate(WRAP_ARROWS)
+         if hit == PLAYER:
+            player.kill(DEAD)
+         elif hit == BOWMAN:
+            Sprite.listKillAt(bowmen, arrow.x, arrow.y, DEAD)
+         elif hit == 'STUCK':
+            arrow.kill()
 
-  bowmen = Sprite.listRemoveTheDead(bowmen)
-  arrows = Sprite.listRemoveTheDead(arrows)
+   bowmen = Sprite.listRemoveTheDead(bowmen)
+   arrows = Sprite.listRemoveTheDead(arrows)
 
-  if bowmen == []:
-    frame.shout(Frame.GREEN, 'YOU WIN')
-    break
+   if bowmen == []:
+      frame.shout(Frame.GREEN, 'YOU WIN')
+      break
 
-  bowman = random.choice(bowmen)
-  vx     = random.randint(-1,1)
-  vy     = random.randint(-1,1)
-  if vx != 0 or vy != 0:
-    arrow = Sprite(frame, Frame.RED, ARROW,
-                   bowman.x + vx, bowman.y + vy)
-    arrow.setSpeed(vx, vy)
-    arrows.append(arrow)
+   bowman = random.choice(bowmen)
+   vx     = random.randint(-1,1)
+   vy     = random.randint(-1,1)
+   if vx != 0 or vy != 0:
+      arrow = Sprite(frame, Frame.RED, ARROW,
+                     bowman.x + vx, bowman.y + vy)
+      arrow.setSpeed(vx, vy)
+      arrows.append(arrow)
